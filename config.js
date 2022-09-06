d7e18d4e567aa1449e1bf39054ad605ec485249f
GansKatriel <112304004+GansKatriel@users.noreply.github.com>
2022-09-06 17:14:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -44,8 +44,8 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 /*============== SOCIAL ==============*/
 
 // Wajib diisi ya kak!, kalo ga punya biarin aja, biar ada creditsnya :v
-global.sig = 'https://www.instagram.com/kontol_elite'
-global.sgh = 'https://github.com/Jikarinka'
+global.sig = 'https://www.instagram.com'
+global.sgh = 'https://github.com/Rieltod'
 global.sgc = 'https://chat.whatsapp.com/FF1Bd1U0FHu42rfz6uplbn'
 global.sdc = " "
 global.snh = 'https://nhentai.net/g/365296/'
@@ -54,23 +54,23 @@ global.snh = 'https://nhentai.net/g/365296/'
 global.pdana = '085334930628'
 global.povo = '085712420674'
 global.pgopay = '085712420674'
-global.ppulsa = '085334930628'
+global.ppulsa = '083813417529'
 global.ppulsa2 = '081336990781'
 global.psaweria = '-'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6281336990781'
-global.nomorown = '6285334930628'
-global.namebot = 'Kanna BOT'
-global.nameown = 'ᴠᴀɴᴇꜱꜱᴀ-ᴅᴇꜱᴜ'
+global.nomorbot = '083813417529'
+global.nomorown = '6282146218274'
+global.namebot = 'Khuruhi-Md'
+global.nameown = 'Riellganzz'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6285334930628'],
-  ['6285334930628'],
-  ['18126338970'],
-  ['6285334930628', 'ᴠᴀɴᴇꜱꜱᴀ-ᴅᴇꜱᴜ', true]
+  ['6282146218274'],
+  ['6282146218274'],
+  ['6282146218274'],
+  ['6282146218274', 'Rielganzz', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
